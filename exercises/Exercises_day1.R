946754c8f46e8f5a33f5dadccba7aac886536030
Jentoft <coo@ssb.no>
2021-01-11 12:48:42 +0100

    Added exercise folder

---
+++ exercises/Exercises_day1.R
@@ -0,0 +1,91 @@
+# R introduction course
+# Day 1
+
+
+#### Exercise 1 ####
+
+
+# a) What is the sum of 4499 and 35?
+
+
+
+# b) Find the help file for the function sqrt(). What does it do?
+
+
+
+# c) What does the function floor() do? What is the answer to floor(2.48)
+
+
+
+# d) Create an object which contains a string with the name of an oblast (region). Print the object to the console (Run)
+
+
+
+# e) Paste the oblast object together with the string "oblast"
+
+
+
+# f) Create a vector with 3 different numbers (integers). Call this vector: employees
+
+
+
+# g) Find the help file for the function length()
+
+
+
+# h) Check the length of the vector, employees, with the function length()
+
+
+
+
+
+
+
+
+
+
+#######################################################################################
+
+#### Exercise 2 ####
+
+# Use the following vector for exercise 2a-b (or the one you created in the previous exercise)
+employees <- c(3, 20, 14)
+  
+# a) Write a condition/test to see which elements in the vector, employees, are less than 5
+
+
+
+# b) Write a test to see if the vector, employees, contains the number 9
+
+
+
+# c) Fetch the library tidyverse
+
+
+
+
+# d) Open the dataset "employees.csv" without reading it in. 
+#    What seperator does it use?
+# Note: The dataset is in the data folder of the repository. I contains the number 
+#    of employees in Ukraine by year in industry groups.
+# Hint: Find the dataset under Files and click on it. Choose "View File"
+
+
+
+# e) Read in the dataset "employees.csv"
+# Hint: Remember to use " " around the path and file name.
+
+
+
+# f) Look at the dataset. How many variables does it have?
+
+
+
+# g) What type of variable is Industry?
+
+
+
+# h) What is the smallest number of employees in any industry group in 2017?
+
+
+
